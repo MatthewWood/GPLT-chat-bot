2186ddae13c1eef4830d09e7a9c62dc0405efcb1
Wood <matthwood@lm-lhr-00904656.corp.ebay.com>
2017-06-26 18:02:49 +0100

    end of day commit, hello working, working on cantt print a label

--- modules/chatbot/scripts/chatbotService.js
+++ modules/chatbot/scripts/chatbotService.js
@@ -1,5 +1,42 @@
 var apiai = require('apiai');
+var processor = apiai('5f05b245897e45ae9d0e7e114a55719f');
 
-module.exports.processMessage = function (message) {
-    return message;
+const uuidv1 = require('uuid/v1');
+
+module.exports.processMessage = function (req, res) {
+
+    var message = req.body.message;
+
+    var sessionId = uuidv1();
+    var request = processor.textRequest(message, {
+        sessionId: sessionId
+    });
+
+    request.on('response', function (response) {
+        console.log(response.result.fulfillment.speech);
+        return res.send(processAiResponse(response));
+    });
+
+    request.on('error', function (error) {
+        var date = new Date();
+        console.log(date.toDateString() + ': ' + JSON.stringify(error));
+        return res.send(date.toDateString() + ': ' + 'Error from APIAI!');
+    });
+
+    request.end();
+};
+
+var processAiResponse = function (aiResponse) {
+    if (aiResponse.result.fulfillment.speech === '') {
+        // TODO make this into normal foprloop
+        aiResponse.result.fulfillment.messages.forEach(function (responseMessage) {
+            if (responseMessage.type === 4) {
+                if (responseMessage.payload.problem !== '') {
+                    return responseMessage.payload.problem;
+                }
+            }
+        });
+    } else {
+        return aiResponse.result.fulfillment.speech;
+    }
 };
--- routes/chatbot.js
+++ routes/chatbot.js
@@ -2,8 +2,8 @@ var express = require('express');
 var service = require('../modules/chatbot/scripts/chatbotService');
 var router = express.Router();
 
-router.post('/message', function (req, res, next) {
-    res.send(service.processMessage(req.body.message));
+router.post('/message', function (req, res) {
+    service.processMessage(req, res);
 });
 
 module.exports = router;
